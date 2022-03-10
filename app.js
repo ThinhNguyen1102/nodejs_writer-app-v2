acb2595ccc503431757de4cdec7776f9106c6957
Thinh <thinhtheblues1102@gmail.com>
2022-03-10 20:36:53 +0700

    Minor change

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ window.addEventListener("load", function () {
       if (isEdit) {
         a_node.setAttribute(
           "href",
-          `https://thinhnguyen1102.github.io/text-edit.html?edit=true&id=${noteId}`
+          `https://thinhnguyen1102.github.io/writer-web-app/text-edit.html?edit=true&id=${noteId}`
         );
       } else {
         a_node.setAttribute(
